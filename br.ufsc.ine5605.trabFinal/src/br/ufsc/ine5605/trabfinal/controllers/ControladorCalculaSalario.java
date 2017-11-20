c5c6ff55dad5ee324ed34d64cde9ba6298de6a8f
Jonathan <giam.cardoso@gmail.com>
2017-11-20 18:45:23 -0200

    Add files via upload

--- br.ufsc.ine5605.trabFinal/src/br/ufsc/ine5605/trabfinal/controllers/ControladorCalculaSalario.java
+++ br.ufsc.ine5605.trabFinal/src/br/ufsc/ine5605/trabfinal/controllers/ControladorCalculaSalario.java
@@ -1,20 +1,23 @@
 package br.ufsc.ine5605.trabfinal.controllers;
 
-import br.ufsc.ine5605.trabfinal.display.TelaCalculaSalario;
+//import br.ufsc.ine5605.trabfinal.display.TelaCalculaSalario;
+import br.ufsc.ine5605.trabfinal.objects.Funcionario;
+import br.ufsc.ine5605.trabfinal.persistence.FuncDAO;
 
 public class ControladorCalculaSalario extends Controlador {
-    private TelaCalculaSalario telaCalcSal;
+  //  private TelaCalculaSalario telaCalcSal;
     private static ControladorCalculaSalario ctrlCalcular;
+    private ControladorFuncionario ctrlFunc;
 
     public ControladorCalculaSalario() {
         super();
-        telaCalcSal = new TelaCalculaSalario(this);
+        //telaCalcSal = new TelaCalculaSalario(this);
 		
     }
 
     @Override
     public void inicia() {
-        telaCalcSal.setVisible(true);
+//        telaCalcSal.setVisible(true);
     }
 
     public static ControladorCalculaSalario getCtrlCalcular() {
@@ -27,5 +30,23 @@ public class ControladorCalculaSalario extends Controlador {
     public static void setCtrlCalcular(ControladorCalculaSalario ctrlCalcular) {
         ControladorCalculaSalario.ctrlCalcular = ctrlCalcular;
     }   
+
+  /*  public void recebendoDados(String matricula, String faltas, String horasExtras) throws Exception {
+        if(ctrlFunc.validaFuncionarioExiste(matricula)) {
+            if(faltas.matches("[0-9]")) {
+                if(horasExtras.matches("^\\d+(\\.\\d\\d)*$")) {
+                  return "text";
+                } else {
+                    throw new IllegalArgumentException("Horas Extras obrigatóriamente devem ser dígitos");
+                }
+            } else {
+                throw new IllegalArgumentException("Número de faltas devem ser dígitos \nCaso não possua faltas digite 0");
+            }
+        } else {
+            throw new IllegalArgumentException ("Funcionário Inexistente, digite outra Matrícula");
+        }
+    } */
+    
+    
 
 }
